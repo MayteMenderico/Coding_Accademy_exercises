4a6ff92f4dc328b1c2d1b7a5700fd8e26a4ee492
MayteMenderico <59563242+MayteMenderico@users.noreply.github.com>
2020-03-09 09:56:59 +0100

    Update my_putnbr.c

--- Exercises C - Day 3/ex_06/my_putnbr.c
+++ Exercises C - Day 3/ex_06/my_putnbr.c
@@ -1,18 +1,21 @@
-#include<unistd.h>
+#include <unistd.h>
 
-void my_putchar(char);
-    void my_putnbr(int n)
+void my_putchar(char c);
 
-	int main()
-	
-    {
-      int m = 42;
-      int n = -42;
-  my_putchar("42%d, -42%d\n");
-  
-  return 
+
+void my_putnbr(int nb)
+{
+    if (nb < 0) {
+        char c = '-';
+        nb = nb * (-1);
+        write(1, &c, 1);
+    }
+	if (nb > 10)
+	{
+		my_putnbr(nb / 10);
+	}
+	nb %= 10;
+	my_putchar(nb + '0');
 }
-
-
 
 
