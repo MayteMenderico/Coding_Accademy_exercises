22928ef9805b2b4c9bd1b189dfd30f38c28da62e
Mayte <mayte.ferreira-menderico@epitech.eu>
2020-04-03 13:11:27 +0200

    changes in the main.c

--- C  Exercise - Day 9-10/ex_02/main.c
+++ C  Exercise - Day 9-10/ex_02/main.c
@@ -5,10 +5,27 @@ int main() {
 	int **table = (int **)malloc(4 * sizeof(int *));
 	for(int i = 0; i < 4; i++) {
 		table[i] = (int *)malloc(4 * sizeof(int));
-		for(int j = 0; j < 4; j++) {
-			table[i][j] = j;
-		}
 	}
+
+	table[0][0] = 0;
+	table[0][1] = 0;
+	table[0][2] = 1;
+	table[0][3] = 1;
+
+	table[1][0] = 0;
+	table[1][1] = 0;
+	table[1][2] = 1;
+	table[1][3] = 1;
+
+	table[2][0] = 2;
+	table[2][1] = 2;
+	table[2][2] = 3;
+	table[2][3] = 3;
+
+	table[3][0] = 2;
+	table[3][1] = 2;
+	table[3][2] = 3;
+	table[3][3] = 3;
 
 	algo_line(table, 0);
 	algo_column(table, 0);
@@ -16,3 +33,4 @@ int main() {
 
 	return 0;
 }
+
